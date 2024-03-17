5586e8d9d4d54f0392190918b215787ba0a80992
zryshuaige <151923106+zryshuaige@users.noreply.github.com>
2024-03-17 22:50:29 +0800

    Create task.js

---
+++ task.js
@@ -0,0 +1,11 @@
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+
+    if (taskInput.value !== '') {
+      var newTask = document.createElement('li');
+      newTask.textContent = taskInput.value;
+      taskList.appendChild(newTask);
+      taskInput.value = '';
+    }
+  }
